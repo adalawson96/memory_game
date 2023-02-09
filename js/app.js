a89040237fe9a9b4df3152bbf9c03468180cc216
adalawson96 <adalawson96@gmail.com>
2023-02-09 01:25:31 +0100

    add logic for match/incorrect in js

--- js/app.js
+++ js/app.js
@@ -19,43 +19,55 @@ const animals = [
   { imgSrc: "./images/game-img/sheep.jpg", name: "sheep", number: "9"}
 ];
 
-    /*----- state variables -----*/
+/*----- state variables -----*/
 //declare the section (cards are located)
 const section = document.querySelector('section');
 let lastClicked = '';
+let firstCard;
+let secondCard;
+let playerWrong = 3;
+let match;
 
-  /*----- cached elements  -----*/
-//need play button
+/*----- cached elements  -----*/
 const playAgainButton = document.querySelector('button');
-
+const messageEl = document.querySelector('h2');
 //declare the wrong count
 const playerWrongCount = document.querySelector('span');
-//max number of wrongs player can have let because need to modify the value
-let playerWrong = 8;
 //change text of the #
 playerWrongCount.innerHTML = playerWrong;
 
 /*----- event listeners -----*/
-//add event listener / get element by ID
-// playAgainButton.addEventListener('click', initialize);
-// initialize();
+playAgainButton.addEventListener('click', initialize);
 
 /*----- functions -----*/
-// function initialize(){
-//   winner = null;
-//   render();
-// }
+function initialize(){
+  winner = '';
+  render();
+}
 
-//create the randomize
+//create the randomize:
 const randomize = () => {
   animals.sort(() => Math.random() -0.5);
   return animals;
 };
 
-//card generator function
+//card generator function:
 const cardGenerator = () => {
   const cardData = randomize();
   //generate html for ALL of the cards so run through the array using .forEach()
+  const handleClick = function (evt){
+    if (!firstCard){
+      firstCard = evt.target
+    } else {
+      secondCard = evt.target
+    };
+    // console.log(firstCard);
+    // console.log(secondCard);
+    evt.target.classList.toggle('toggleCard');
+    if (firstCard && secondCard) {
+      checkCards(evt, handleClick);
+    };
+  };
   cardData.forEach((item) => {
     //use document.createElement() to create the html div:
     const card = document.createElement('div');
@@ -65,9 +77,7 @@ const cardGenerator = () => {
     const backSide = document.createElement('div');
     card.classList.add('card');
     card.setAttribute('number', item.number);
-    // card.classList.add(item.name);
     card.setAttribute('name', item.name);
-    // face.classList.add('face');
     backSide.classList ='back-side';
     //attach the informaiton to the cards by grabbing section/ array: 
     face.src = item.imgSrc;
@@ -76,94 +86,93 @@ const cardGenerator = () => {
     card.appendChild(face);
     section.appendChild(card);
     //card flip by grabbing card: //source: https://jefferson-cuartas.medium.com/how-to-create-a-flip-card-effect-using-javascript-767dd945210c
-    card.addEventListener('click', function(evt){
-      evt.target.classList.toggle('toggleCard');
-      matchingCards(evt)
-    });
+    card.addEventListener('click', handleClick);
   });
 };
 
 //check cards if they match:
-function matchingCards(evt) {
-  const clickedCard = evt.target;
-  // console.log(evt.target);
-  console.log(clickedCard.parentElement.getAttribute('name'));
-  const flippedCards = document.querySelectorAll('.flipped');
-  clickedCard.classList.add('flipped');
-  console.log(clickedCard.parentElement);
-  
-  //if two cards have been flipped then check if faces match with 'name' if they do return match, if they don't reutrn wrong the cards stay face up and cannot be clicked on anymore:
-  if (lastClicked != '' && 
-  lastClicked.parentElement.getAttribute('name') === clickedCard.parentElement.getAttribute('name') && 
-  lastClicked.parentElement.getAttribute('number') != clickedCard.parentElement.getAttribute('number')) {
+function checkCards(evt, handleClick) {
+  let firstAnimal = firstCard.parentElement.getAttribute('name');
+  let secondAnimal = secondCard.parentElement.getAttribute('name');
+  console.log('first', firstCard.parentElement);
+  console.log('second', secondCard.parentElement);
+  //if two cards have been flipped then check if faces match with 'name' if they do return match & cards stay face up and cannot be clicked on anymore, if they don't reutrn wrong & flip card back (use setTimeout):
+  if (firstAnimal === secondAnimal){
     console.log('match');
-    // flippedCards.forEach((card) => {
-    //   card.classList.remove('flipped');
-    //   card.getElementsByClassName.pointerEvents = "none";
-    // });
+    match++;
+    //stay facing up:
+    firstCard.parentElement.removeEventListener('click', handleClick);
+    secondCard.parentElement.removeEventListener('click', handleClick);
+    // render winner: 
+    // if (toggle.length === 16){
+    // restart('you win');
+    //   alert('you win');
+    // }
   } else {
     console.log('incorrect');
-    // flippedCards.forEach((card) => {
-    //   card.classList.remove('flipped');
-    //   setTimeout(() => card.classList.remove('toggleCard'), 1000);
-    // });
-    lastClicked = clickedCard;
+    playerWrong--;
+    //wrong cards flip back / add timeout:
+    firstCard.parentElement.classList.toggle('toggleCard');
+    secondCard.parentElement.classList.toggle('toggleCard');
   }
-    //if the card faces match the cards stay face up and cannot be clicked on anymore // //if the card faces do not match //time delay for card flip back if not a match use setTimeout:
+  //logs out the # of wrong 
+  playerWrongCount.textContent = playerWrong;
+  if (playerWrong === 0){
+    // restart('you lose');
+    alert('you lose');
+  };
+  firstCard = null;
+  secondCard = null;
+};
+    
+//restart the game: 
+const restart = (text) => {
+  let cardData = randomize();
+  let faces = document.querySelectorAll('.face');
+  let cards = document.querySelectorAll('.card');
+  section.style.pointerEvents = 'none';
+  cardData.forEach((item, index) => {
+    cards[index].classList.remove('toggleCard');
+    //randomize cards after lose:
+    setTimeout(() => {
+    cards[index].style.pointerEvents = 'all';
+    faces[index].src = item.imgSrc;
+    cards[index].setAttribute('name', item.name);
+    section.style.pointerEvents = 'all';
+    }, 1000);
+  });
+  playerWrong = 3;
+  playerWrongCount.textContent = playerWrong;
 };
 
-// function unflipCards() {
-  
-  // };
-  
-  // setTimeout(function () {
-    //   wrongMatch.play();
-    //   toggle.classList.remove('flipped');
-    // }, 1000
-    
-    
-    //count/display/ RENDER the number of 'wrongs'  --> add one each time  (h1 tag w/class playerWrongCount):
-    // function render (){
-      //   renderMessage();
-      // //hiding/showing play again button 
-      //   playAgainBtn.disabled = !winner;
-      // }
-      
-      //check to see if the game is won --> if all cards are face up --> win:
-      
-      //create restart--> when 8 wrong flip all cards back:
-      // function resetBoard() {
-        
-        // };
-        
-        //reset player lives:
-        // function resetWrong() {};
-        
-        // reset game/randomize cards by clicking play again:
-        
-        // // function renderMessage (){
-          // //   if (winner < '8'){
-            // //     messageEl.innerText = "you win!";
-            // //   } else { 
-  //       (loser > '8'){
-    // //     messageEl.innerText = "you loose";
-    //      } else {
-      // //   //Game is in play in this condition
-      // //   messageEl.innerHTML = `<span innerText="wrong: 
-      // //   ${playerWrong['']}">${playerWrongCount[playerWrong].toUpperCase()}</span>'s Wrong`;
-      // //   }
-      //      }
-      
-      
-      //restart
-      const restart = () => {
-        let cardData = randomize();
-        let faces = document.querySelectorAll('.face');
-        let cards = document.querySelectorAll('.card');
-        cardData.forEach((item, index) => {
-          cards[index].classList.remove('toggleCard');
+//visualize all state in the DOM
+function render() {
+  renderMessage();
+  // renderControls();
+  //hiding/showing play again button 
+  playAgainButton.disabled = !winner;
+}
 
-        });
-      };
-      
-      cardGenerator();
+//Handle a player clicking the replay button:
+//Mesage of who wins but this would be seperate?: 
+function renderMessage() {
+  if (winner === null) {
+    messageEl.innerText = 'loose';
+  }else if (winner) { 
+    messageEl.innerText = 'win!';
+  }else {
+    messageEl.innerHTML = 'keep trying';
+  };
+};
+
+//makes button hide:
+// function renderControls() {
+//   playAgainButton.style.visibility = winner? 'visible':'hidden';
+// }
+
+cardGenerator();
+  
+  
+// check to see if the game is won --> if all cards are face up --> win:
+// create restart--> when 8 wrong flip all cards back:
+// function resetBoard() {}
